976b2da175d2acd5e34b0c1cde2b3342fd828bfd
Siddharth Pandey <siddharthpandey@Siddharths-MacBook-Pro.local>
2019-04-29 11:45:04 -0400

    skeleton

---
+++ src/main/StudentReservation.java
@@ -0,0 +1,17 @@
+package main;
+import javax.swing.*;
+
+public class StudentReservation {
+
+    public static void main(String[] args){
+
+
+    }
+
+
+    public int sum(int a,int b ){
+
+        return a+b;
+
+    }
+}
